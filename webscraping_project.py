be342c3c3e5b8cbe8e5a7aa0486dc409501a6e7d
Nathan Selig <nathanselig@gmail.com>
2022-10-02 23:53:16 -0400

    in this over am i going insane i have completed base functinality but still lots of QOL avaible but you can analylis the data with python so that is good

--- webscraping_project.py
+++ webscraping_project.py
@@ -12,28 +12,42 @@ print('this program is used to not only see the value of stocks but also compare
 
 #region functions
 def stock_information(page, stock_name):
-
     user_soup = BeautifulSoup(page.content, 'html.parser') 
     #region price of stock
     stock_price  = user_soup.find('fin-streamer', class_ = 'Fw(b) Fz(36px) Mb(-4px) D(ib)')
     stock_info = user_soup.find('table', class_ = 'W(100%)')
     time_of_data = user_soup.find('div', id = 'quote-market-notice')
     
-
-    print(stock_price['value'], '  ')
     print('')
-    #print(stock_info.get_text())
-    #print('')
+    print(stock_price['value'], '  ')
     print(time_of_data.get_text())
+    print('')
     
-    load_data(stock_name)
+    info_DB = pd.DataFrame({
+        'price':stock_price['value'],
+        'time':time_of_data.get_text()
+    },index = [0])
+    
+    return info_DB
 
-def getDB():
-    file = 'commonstocks.csv'
-    database = pd.read_csv(file, index_col = 0)
 
 def compare(input):
     starhill_variable = str(input).split()
+    
+    user_page1 = requests.get(f'https://finance.yahoo.com/quote/{starhill_variable[0]}')
+    user_page2 = requests.get(f'https://finance.yahoo.com/quote/{starhill_variable[1]}')
+    
+    stock1_info = stock_information(user_page1,"")
+    stock2_info = stock_information(user_page2,"")
+    commonstocks1 = load_common(starhill_variable[0])
+    commonstocks2= load_common(starhill_variable[1])
+    
+    stock1_all = pd.concat([stock1_info,commonstocks1], axis = 'columns')
+    stock2_all = pd.concat([stock2_info,commonstocks2], axis = 'columns')
+    
+    joint_info = pd.concat([stock1_all, stock2_all])
+    
+    print(joint_info)
     
 def load_data(stock_name):
     directory = 'stockdata'
@@ -115,7 +129,7 @@ def load_data(stock_name):
                 low.append(i[3])
                 close.append(i[4])
             
-            
+                        
             data_month = pd.DataFrame({
                 'Date':date,
                 'High':high,
@@ -128,6 +142,24 @@ def load_data(stock_name):
             
             
 
+def load_common(stock_name):
+    file = 'commonstocks.csv'
+    database = pd.read_csv(file, index_col = 0)
+    #make for loop for each line in file
+    for data in open(file):
+    #split the string and find symbol
+        data = data.split(',')
+    #if symbol == stock_name return row as pd DB
+        if data[2].strip('\n') == stock_name:
+            return pd.DataFrame({
+                'rank':data[0],
+                'name':data[1],
+                'symbol':data[2].strip('\n')                
+                
+                },index = [0])
+    
+    
+    
 #* page format etfs https://finance.yahoo.com/quote/%5E[stock name]?p=%5E[stock name]
 #* page format stocks /quote/[stock name]
 #* page format currency
@@ -145,7 +177,7 @@ print(time)
 print('')
 
 #* ask user what stocks they want to look at
-print('what are you looking for: \nstock\netf\n')
+print('what are you looking for: \nstock\netf\ncompare')
 choice = input("")
 
 match choice:
@@ -157,7 +189,7 @@ match choice:
         user_stock = input(f'what {choice} would you like to look at: ').upper()
         user_page = requests.get(f'https://finance.yahoo.com/quote/%5E{user_stock}?p=%5E{user_stock}')
         stock_information(user_page)
-    case 'compare stocks':
+    case 'compare':
         #TODO compare
         user_stock = input(f'what {choice} would you like to compare: ').upper()
         compare(user_stock)
